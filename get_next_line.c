5e57d4af13af01b2f05880c8d2efb922412fbb38
Falarcon <fer_min_nito@hotmail.com>
2023-02-23 13:15:09 +0100

    función read_next_line añadida

--- get_next_line.c
+++ get_next_line.c
@@ -140,5 +140,30 @@ char	*ft_strchr(const char *s, int c)
 }
 
 char	*read_next_line(int fd, char *buffer, char **line, int *bytes_read)
+{
+	char	*new_line_ptr;
+	char	*temp;
 
+	*bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (*bytes_read <= 0)
+		return (NULL);
+	buffer[*bytes_read] = '\0';
+	new_line_ptr = ft_strchr(buffer, '\n');
+	if (new_line_ptr != NULL)
+	{
+		*new_line_ptr = '\0';
+		*line = ft_strjoin(*line, buffer);
+		temp = ft_strdup(new_line_ptr + 1);
+		ft_memcpy(buffer, temp, ft_strlen(temp) + 1);
+		free(temp);
+	}
+	else
+	{
+		*line = ft_strjoin(*line, buffer);
+		buffer[0] = '\0';
+	}
+	if (*line == NULL)
+		return (NULL);
+	return (*line);
+}
 
